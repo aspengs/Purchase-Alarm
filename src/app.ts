22bf8568a04877b94abeb178b18e0bc526580252
aspengs <jmf@grayshirts.com.ar>
2018-11-30 01:32:26 -0300

    added node-schedule

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,21 @@ import path = require('path');
 import morgan = require('morgan');
 import * as mongoose from "mongoose";
 let config = require('../config/config');
+let schedule = require('node-schedule');
 
 class App {
 
   public app: express.Application;
   public routePrv: Routes = new Routes();
   public mongoUrl: string = config.database.url;
+  public job: any;
 
   constructor() {
     this.app = express();
     this.config();
     this.routePrv.routes(this.app);
     this.mongoSetup();
+    this.startJob();
   }
 
   private config(): void {
@@ -29,18 +32,17 @@ class App {
     //support application/x-www-form-urlencoded post data
     this.app.use(bodyParser.urlencoded({ extended: false }));
 
-    // if (process.env.NODE_ENV === 'production') {
-    //   // Serve any static files
-    //   this.app.use(express.static(path.join(__dirname, 'client/build')));
-    //   // Handle React routing, return all requests to React app
-    //   this.app.get('*', function (req, res) {
-    //     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    //   });
-    // }
   }
+
   private mongoSetup(): void {
     // mongoose.Promise = global.Promise;
     mongoose.connect(this.mongoUrl, { useNewUrlParser: true });
+  }
+  
+  private startJob(): void {
+    this.job = schedule.scheduleJob("*/1 * * * *", function() {
+      console.log('This runs every 5 minutes');
+  });
   }
 }
 
