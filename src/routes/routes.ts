5e83c5df1127d64c3f0d96c5ee1dc534c92d3f63
aspengs <jmf@grayshirts.com.ar>
2018-11-29 21:51:45 -0300

    read ENV vars from heroku

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -9,10 +9,10 @@ export class Routes {
 
         if (process.env.NODE_ENV === 'production') {
             // Serve any static files
-            app.use(express.static(path.join(__dirname, 'client/build')));
+            app.use(express.static(path.join(__dirname, '../../client/build')));
             // Handle React routing, return all requests to React app
             app.route('*').get(function (req: Request, res: Response)  {
-              res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+              res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
             });
           }
 
