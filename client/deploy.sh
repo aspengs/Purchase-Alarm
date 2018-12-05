30d2c7874e3bcbc71377c754ab1d60fc1192da78
Nicolas Sandoval <mac@nsandoval.com.ar>
2018-12-05 17:31:55 -0300

    Fix deploy.sh with yarn and tsc

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,8 +1,9 @@
 rm -rf build
-npm install
+yarn
+tsc
 npm run build
 echo 'Deploying static site to S3 bucket:' $1
 aws s3 rm s3://$1 --recursive
 aws s3 sync --acl public-read-write ./build s3://$1
-#aws cloudfront create-invalidation --distribution-id $2
+#historyaws cloudfront create-invalidation --distribution-id $2
 echo 'Done!'
