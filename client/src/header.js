3d068801153d90d078953710211e262cbdf2d067
aspengs <jmf@grayshirts.com.ar>
2018-12-06 11:34:21 -0300

    fix header class and some styles

--- client/src/header.js
+++ client/src/header.js
@@ -1,20 +1,11 @@
-import React from 'react';
+import React from "react";
 
 class Header extends React.Component {
-
-
-  
   render() {
     return (
-       
-        <header className="headerGarba">
-            <nav class="navbar navbar-light">
-        
-                <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""/>
-            
-            </nav>
-        </header>
-       
+      <header className="headerGarba">
+        <nav className="navbar navbar-light" />
+      </header>
     );
   }
 }
