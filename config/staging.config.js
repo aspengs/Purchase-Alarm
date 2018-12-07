1f097e2e1928d614c7758022a5b5bafd9e796993
Nicolas Sandoval <mac@nsandoval.com.ar>
2018-12-07 16:50:48 -0300

    Add missing staging config

---
+++ config/staging.config.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = (ROOT_PATH, timeoutRatio) => {
+  var config = {
+    services: {
+      checkout: {
+        core: {
+          base_url: "http://api-global-ci.garbarino.com",
+          resource: "/carts",
+          timeout: 2000 * timeoutRatio,
+          ttl: 1
+        },
+        monitor: {
+          url: "http://api.garbarino.com/payments/monitor.php",
+          api_key: "2d3d3655bf6aa36615e3ea79d605e728b8bc379a"
+        }
+      },
+      telegram_bot: {
+        token: "655824168:AAH78FIQFcwHzOJmP6PDXimwqpBzctoYRkw"
+      }
+    },
+    database: {
+      url: "mongodb://mongo-staging.garba/checkout-alarms"
+    }
+  };
+  return config;
+};
